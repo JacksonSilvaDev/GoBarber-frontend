4dc5ea469fb15b8eba04e56de1e9e9557e491d5b
Jackson Silva <jacksonsilvadev@gmail.com>
2020-09-01 11:56:11 -0300

    Added type for Toast and auto remove

---
+++ src/components/ToastContainer/index.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+
+import Toast from './Toast';
+
+import { Container } from './styles';
+import { ToastMessage } from '../../hooks/Toast';
+
+interface ToastContainerProps {
+  messages: ToastMessage[];
+}
+
+const ToastContainer: React.FC<ToastContainerProps> = ({ messages }) => {
+  return (
+    <>
+      <Container>
+        {messages.map(message => (
+          <Toast key={message.id} message={message} />
+        ))}
+      </Container>
+    </>
+  );
+};
+
+export default ToastContainer;
